d03c6d1a3fe4d2be2593fd9abfb6339590677e99
nooralamwaj <nooralamwaj@gmail.com>
2025-07-04 21:36:07 +0400

    k

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Services() {
   const services = [
     { title: 'Land Transport', description: 'Land Transportation division has been providing regular full loads and services within the GCC and Levant countries. It maintains flexible and tailor made road services to meet customer needs.' },
-    { title: 'Ocean Freight Services', description: 'Noor Al Amwaj handles movement of cargo from/to any global origin/destination, be it LCL or FCL. With its own Container Freight Station (CFS) and Supply Chain facilities, As a leading NVOCC, we have been maintaining an excellent relationship with leading carriers, ensuring competitive rates and services' },
+    { title: 'Ocean Freight Services', description: 'Noor Al Amwaj handles movement of cargo from/to any global origin/destination, be it LCL or FCL. We have been maintaining an excellent relationship with leading carriers, NVOCC ensuring competitive rates and services' },
     { title: 'Air Cargo', description: 'Noor Al Amwaj offers effective Air Cargo services. Our Air Freight team offers complete logistical solutions and is a trusted partner for Air Cargo Management. Our expertise in operations, customs regulations, licenses & consular documentation help achieve optimum results.' },
   ];
 
